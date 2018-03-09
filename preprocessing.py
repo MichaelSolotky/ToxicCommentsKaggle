23f6e7a7b3ae13c3e4bc206e8bd935c88183d27b
MichaelSolotky <25284863+MichaelSolotky@users.noreply.github.com>
2018-03-10 02:40:49 +0300

    trained neive bayes and achieved baseline

--- preprocessing.py
+++ preprocessing.py
@@ -1,16 +1,21 @@
-from nltk import WordNetLemmatizer, stopwords
+from nltk import WordNetLemmatizer
+from nltk.corpus import stopwords
 import re
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from textblob import TextBlob
 
 
-def raw_comment_to_tokens_list(comment):
+def preprocess(comment):
+    """
+        Return str with lemmatized words without stop words
+    """
     lemmatizer = WordNetLemmatizer()
-    tokens = TextBlob(train['comment_text'][0]).tags
+    tokens = TextBlob(comment).tokens
     stop = set(stopwords.words('english'))
-    new_comment = []
+    new_comment = ' '
+    normalized_tokens = []
     for token in tokens:
         normalized = lemmatizer.lemmatize(token[0])
         if normalized not in stop:
-            new_comment.append((normalized, token[1]))
-    return new_comment
+            normalized_tokens.append(normalized)
+    return ' '.join(normalized)
